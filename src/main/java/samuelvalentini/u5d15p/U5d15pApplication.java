c170589e238570d074bc1c482c90a6832dd592a6
Samuel-Valentini <samuelvitalia@gmail.com>
2026-04-24 09:50:04 +0200

    first spring project creation

---
+++ src/main/java/samuelvalentini/u5d15p/U5d15pApplication.java
@@ -0,0 +1,13 @@
+package samuelvalentini.u5d15p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d15pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d15pApplication.class, args);
+	}
+
+}
